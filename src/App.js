fd9f816a29e047e43d06f65e5a88ceaf86f89e76
Timothy Nwokeji <nwokejitimoth00@gmail.com>
2022-12-03 23:12:04 +0100

    Initial commit of complete countdown app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Head from './components/header'
+import Time from './components/time'
+import Footer from './components/footer'
+import { useEffect, useState} from 'react'
+import SetTime  from './components/setTime'
+import Setting from  './components/settingTime'
 
-function App() {
+
+
+const App = ()=>{
+
+const [text, setText]= useState("We're Lauching soon.");
+const [permit, setPermit]= useState(true);
+const [curentTime, setCurent] = useState({});
+const [clsNme, setCls] = useState("setting hide");
+const [isReset, setReset] = useState(false);
+const [timing , setTiming] = useState({       
+                seconds:0,
+                minutes: 0,
+                hours:0,
+                days:0
+        });
+useEffect(()=>{
+  if(isReset && sum(Object.values(curentTime)) === 0){
+    setText("Launch");
+  }
+}, [curentTime])
+
+        function sum(array){
+          let value = 0;
+          array.forEach((ele)=>{
+            value+=ele;
+          });
+          return value;
+        }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+  <main>
+    <header>
+    <Head text={text} setTime={timing}  findSum={sum} reset={setReset}/>
+    </header>
+        <Time report={setText} auth={permit} setAuth={setPermit} time={timing} current={setCurent}/>
+        {/* <Time/> */}
+        <Setting name={clsNme} setName={setCls} setTime={setTiming}/>
+        <SetTime setcl={setCls} />
+  </main>
+        <Footer/>
+    </>
 
-export default App;
+  
+  )
+}
+export default App
